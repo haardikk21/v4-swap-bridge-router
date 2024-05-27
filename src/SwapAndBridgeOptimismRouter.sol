1b3149188f25857b92a90e4b63026a421b74d47f
Haardik <haardik@haardik.dev>
2024-05-27 11:34:23 -0400

    add tests

---
+++ src/SwapAndBridgeOptimismRouter.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {CurrencySettleTake} from "v4-core/libraries/CurrencySettleTake.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {IERC20Minimal} from "v4-core/interfaces/external/IERC20Minimal.sol";
+import {TransientStateLibrary} from "v4-core/libraries/TransientStateLibrary.sol";
+
+interface IL1StandardBridge {
+    function depositETHTo(
+        address _to,
+        uint32 _minGasLimit,
+        bytes calldata _extraData
+    ) external payable;
+    function depositERC20To(
+        address _l1Token,
+        address _l2Token,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes calldata _extraData
+    ) external;
+}
+
+contract SwapAndBridgeOptimismRouter is Ownable {
+    using CurrencyLibrary for Currency;
+    using CurrencySettleTake for Currency;
+    using TransientStateLibrary for IPoolManager;
+
+    IPoolManager public immutable manager;
+    IL1StandardBridge public immutable l1StandardBridge;
+
+    mapping(address l1Token => address l2Token) public l1ToL2TokenAddresses;
+
+    struct CallbackData {
+        address sender;
+        SwapSettings settings;
+        PoolKey key;
+        IPoolManager.SwapParams params;
+        bytes hookData;
+    }
+
+    struct SwapSettings {
+        bool bridgeTokens;
+        address recipientAddress;
+    }
+
+    error CallerNotManager();
+    error TokenCannotBeBridged();
+
+    constructor(
+        IPoolManager _manager,
+        IL1StandardBridge _l1StandardBridge
+    ) Ownable(msg.sender) {
+        manager = _manager;
+        l1StandardBridge = _l1StandardBridge;
+    }
+
+    function swap(
+        PoolKey memory key,
+        IPoolManager.SwapParams memory params,
+        SwapSettings memory settings,
+        bytes memory hookData
+    ) external payable returns (BalanceDelta delta) {
+        if (settings.bridgeTokens) {
+            Currency l1TokenToBridge = params.zeroForOne
+                ? key.currency1
+                : key.currency0;
+
+            if (!l1TokenToBridge.isNative()) {
+                address l2Token = l1ToL2TokenAddresses[
+                    Currency.unwrap(l1TokenToBridge)
+                ];
+                if (l2Token == address(0)) revert TokenCannotBeBridged();
+            }
+        }
+
+        delta = abi.decode(
+            manager.unlock(
+                abi.encode(
+                    CallbackData(msg.sender, settings, key, params, hookData)
+                )
+            ),
+            (BalanceDelta)
+        );
+
+        uint256 ethBalance = address(this).balance;
+        if (ethBalance > 0)
+            CurrencyLibrary.NATIVE.transfer(msg.sender, ethBalance);
+    }
+
+    function unlockCallback(
+        bytes calldata rawData
+    ) external returns (bytes memory) {
+        if (msg.sender != address(manager)) revert CallerNotManager();
+
+        CallbackData memory data = abi.decode(rawData, (CallbackData));
+
+        BalanceDelta delta = manager.swap(data.key, data.params, data.hookData);
+
+        int256 deltaAfter0 = manager.currencyDelta(
+            address(this),
+            data.key.currency0
+        );
+        int256 deltaAfter1 = manager.currencyDelta(
+            address(this),
+            data.key.currency1
+        );
+
+        if (deltaAfter0 < 0) {
+            data.key.currency0.settle(
+                manager,
+                data.sender,
+                uint256(-deltaAfter0),
+                false
+            );
+        }
+
+        if (deltaAfter1 < 0) {
+            data.key.currency1.settle(
+                manager,
+                data.sender,
+                uint256(-deltaAfter1),
+                false
+            );
+        }
+
+        if (deltaAfter0 > 0) {
+            _take(
+                data.key.currency0,
+                data.settings.recipientAddress,
+                uint256(deltaAfter0),
+                data.settings.bridgeTokens
+            );
+        }
+
+        if (deltaAfter1 > 0) {
+            _take(
+                data.key.currency1,
+                data.settings.recipientAddress,
+                uint256(deltaAfter1),
+                data.settings.bridgeTokens
+            );
+        }
+
+        return abi.encode(delta);
+    }
+
+    function _take(
+        Currency currency,
+        address recipient,
+        uint256 amount,
+        bool bridgeToOptimism
+    ) internal {
+        if (!bridgeToOptimism) {
+            currency.take(manager, recipient, amount, false);
+        } else {
+            currency.take(manager, address(this), amount, false);
+
+            if (currency.isNative()) {
+                l1StandardBridge.depositETHTo(recipient, 0, "");
+            } else {
+                address l1Token = Currency.unwrap(currency);
+                address l2Token = l1ToL2TokenAddresses[l1Token];
+
+                IERC20Minimal(l1Token).approve(
+                    address(l1StandardBridge),
+                    amount
+                );
+                l1StandardBridge.depositERC20To(
+                    l1Token,
+                    l2Token,
+                    recipient,
+                    amount,
+                    0,
+                    ""
+                );
+            }
+        }
+    }
+
+    function addL1ToL2TokenAddress(
+        address l1Token,
+        address l2Token
+    ) external onlyOwner {
+        l1ToL2TokenAddresses[l1Token] = l2Token;
+    }
+
+    receive() external payable {}
+}
---
+++ test/TestSwapAndBridgeOptimismRouter.t.sol
@@ -0,0 +1,405 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, Vm} from "forge-std/Test.sol";
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {IHooks} from "v4-core/interfaces/IHooks.sol";
+import {SwapAndBridgeOptimismRouter, IL1StandardBridge} from "../src/SwapAndBridgeOptimismRouter.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+
+interface IOUTbToken {
+    function approve(address spender, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function faucet() external;
+}
+
+contract TestSwapAndBridgeOptimismRouter is Test, Deployers {
+    using CurrencyLibrary for Currency;
+    using PoolIdLibrary for PoolKey;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    These are events from L1StandardBridge and CrossDomainMessenger
+    //////////////////////////////////////////////////////////////*/
+
+    event ETHDepositInitiated(
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        bytes extraData
+    );
+
+    event ERC20DepositInitiated(
+        address indexed l1Token,
+        address indexed l2Token,
+        address indexed from,
+        address to,
+        uint256 amount,
+        bytes extraData
+    );
+
+    event ETHBridgeInitiated(
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        bytes extraData
+    );
+
+    event ERC20BridgeInitiated(
+        address indexed localToken,
+        address indexed remoteToken,
+        address indexed from,
+        address to,
+        uint256 amount,
+        bytes extraData
+    );
+
+    event SentMessage(
+        address indexed target,
+        address sender,
+        bytes message,
+        uint256 messageNonce,
+        uint256 gasLimit
+    );
+
+    event SentMessageExtension1(address indexed sender, uint256 value);
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 sepoliaForkId = vm.createFork("https://rpc.sepolia.org/");
+
+    SwapAndBridgeOptimismRouter poolSwapAndBridgeOptimism;
+
+    // OUTb = Optimism Useless Token Bridged (ETH Sepolia and OP Sepolia addresses)
+    IOUTbToken OUTbL1Token =
+        IOUTbToken(0x12608ff9dac79d8443F17A4d39D93317BAD026Aa);
+    IOUTbToken OUTbL2Token =
+        IOUTbToken(0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2);
+
+    // L1 Standard Bridge on ETH Sepolia
+    IL1StandardBridge public constant l1StandardBridge =
+        IL1StandardBridge(0xFBb0621E0B23b5478B630BD55a5f21f67730B0F1);
+
+    // Cross Domain Messenger L2 Contract Address
+    address public constant l2CrossDomainMessenger =
+        0x4200000000000000000000000000000000000010;
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SETUP
+    //////////////////////////////////////////////////////////////*/
+
+    function setUp() public {
+        vm.selectFork(sepoliaForkId);
+        vm.deal(address(this), 500 ether);
+
+        // Deploy manager and routers
+        deployFreshManagerAndRouters();
+        poolSwapAndBridgeOptimism = new SwapAndBridgeOptimismRouter(
+            manager,
+            l1StandardBridge
+        );
+
+        // Get some OUTb tokens on L1 and approve the routers to use it
+        OUTbL1Token.faucet();
+        OUTbL1Token.approve(
+            address(poolSwapAndBridgeOptimism),
+            type(uint256).max
+        );
+        OUTbL1Token.approve(address(modifyLiquidityRouter), type(uint256).max);
+
+        // Create the OUTb token mapping on the periphery contract
+        poolSwapAndBridgeOptimism.addL1ToL2TokenAddress(
+            address(OUTbL1Token),
+            address(OUTbL2Token)
+        );
+
+        // Deploy an ETH <> OUTb pool and add some liquidity there
+        (key, ) = initPool(
+            CurrencyLibrary.NATIVE,
+            Currency.wrap(address(OUTbL1Token)),
+            IHooks(address(0)),
+            3000,
+            SQRT_PRICE_1_1,
+            ZERO_BYTES
+        );
+        modifyLiquidityRouter.modifyLiquidity{value: 1 ether}(
+            key,
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: -60,
+                tickUpper: 60,
+                liquidityDelta: 10 ether,
+                salt: bytes32(0)
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    /**
+     * As long as we are running on a fork, we cannot check the OP Sepolia side of things
+     *     So we will only test based on the events being output by the contract
+     *     A separate script file exists which tests on actual Sepolia Testnet and OP Sepolia
+     */
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SWAP ETH FOR OUTb
+                            WITH BRIDGING TO OP
+                            RECIPIENT = SENDER
+    //////////////////////////////////////////////////////////////*/
+
+    function test_swapETHForOUTb_bridgeTokensToOptimism_recipientSameAsSender()
+        public
+    {
+        vm.expectEmit(true, true, true, false);
+        emit ERC20DepositInitiated(
+            address(OUTbL1Token),
+            address(OUTbL2Token),
+            address(poolSwapAndBridgeOptimism),
+            address(this),
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, true, true, false);
+        emit ERC20BridgeInitiated(
+            address(OUTbL1Token),
+            address(OUTbL2Token),
+            address(poolSwapAndBridgeOptimism),
+            address(this),
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessage(l2CrossDomainMessenger, address(0), ZERO_BYTES, 0, 0);
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessageExtension1(address(l1StandardBridge), 0);
+
+        poolSwapAndBridgeOptimism.swap{value: 0.001 ether}(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            SwapAndBridgeOptimismRouter.SwapSettings({
+                bridgeTokens: true,
+                recipientAddress: address(this)
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SWAP ETH FOR OUTb
+                            WITH BRIDGING TO OP
+                            RECIPIENT != SENDER
+    //////////////////////////////////////////////////////////////*/
+
+    function test_swapETHForOUTb_bridgeTokensToOptimism_receipientNotSameAsSender()
+        public
+    {
+        address recipientAddress = address(0x1);
+
+        vm.expectEmit(true, true, true, false);
+        emit ERC20DepositInitiated(
+            address(OUTbL1Token),
+            address(OUTbL2Token),
+            address(poolSwapAndBridgeOptimism),
+            recipientAddress,
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, true, true, false);
+        emit ERC20BridgeInitiated(
+            address(OUTbL1Token),
+            address(OUTbL2Token),
+            address(poolSwapAndBridgeOptimism),
+            recipientAddress,
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessage(l2CrossDomainMessenger, address(0), ZERO_BYTES, 0, 0);
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessageExtension1(address(l1StandardBridge), 0);
+
+        poolSwapAndBridgeOptimism.swap{value: 0.001 ether}(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            SwapAndBridgeOptimismRouter.SwapSettings({
+                bridgeTokens: true,
+                recipientAddress: recipientAddress
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SWAP OUTb FOR ETH
+                            WITH BRIDGING TO OP
+                            RECIPIENT = SENDER
+    //////////////////////////////////////////////////////////////*/
+
+    function test_swapOUTbForETH_bridgeTokensToOptimism_recipientSameAsSender()
+        public
+    {
+        vm.expectEmit(true, true, false, false);
+        emit ETHDepositInitiated(
+            address(poolSwapAndBridgeOptimism),
+            address(this),
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, true, false, false);
+        emit ETHBridgeInitiated(
+            address(poolSwapAndBridgeOptimism),
+            address(this),
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessage(l2CrossDomainMessenger, address(0), ZERO_BYTES, 0, 0);
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessageExtension1(address(l1StandardBridge), 0);
+
+        poolSwapAndBridgeOptimism.swap(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: false,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MAX_SQRT_PRICE - 1
+            }),
+            SwapAndBridgeOptimismRouter.SwapSettings({
+                bridgeTokens: true,
+                recipientAddress: address(this)
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SWAP OUTb FOR ETH
+                            WITH BRIDGING TO OP
+                            RECIPIENT != SENDER
+    //////////////////////////////////////////////////////////////*/
+    function test_swapOUTbForETH_bridgeTokensToOptimism_recipientNotSameAsSender()
+        public
+    {
+        address recipientAddress = address(0x1);
+
+        vm.expectEmit(true, true, false, false);
+        emit ETHDepositInitiated(
+            address(poolSwapAndBridgeOptimism),
+            recipientAddress,
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, true, false, false);
+        emit ETHBridgeInitiated(
+            address(poolSwapAndBridgeOptimism),
+            recipientAddress,
+            0,
+            ZERO_BYTES
+        );
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessage(l2CrossDomainMessenger, address(0), ZERO_BYTES, 0, 0);
+
+        vm.expectEmit(true, false, false, false);
+        emit SentMessageExtension1(address(l1StandardBridge), 0);
+
+        poolSwapAndBridgeOptimism.swap(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: false,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MAX_SQRT_PRICE - 1
+            }),
+            SwapAndBridgeOptimismRouter.SwapSettings({
+                bridgeTokens: true,
+                recipientAddress: recipientAddress
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SWAP ETH FOR OUTb
+                            WITHOUT BRIDGING
+    //////////////////////////////////////////////////////////////*/
+    function test_swapETHForOUTb_dontBridgeTokens() public {
+        uint256 ethBalanceBefore = address(this).balance;
+        uint256 OUTbBalanceBefore = OUTbL1Token.balanceOf(address(this));
+
+        poolSwapAndBridgeOptimism.swap{value: 0.001 ether}(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            SwapAndBridgeOptimismRouter.SwapSettings({
+                bridgeTokens: false,
+                recipientAddress: address(this)
+            }),
+            ZERO_BYTES
+        );
+
+        uint256 ethBalanceAfter = address(this).balance;
+        uint256 OUTbBalanceAfter = OUTbL1Token.balanceOf(address(this));
+
+        assertEq(ethBalanceBefore - ethBalanceAfter, 0.001 ether);
+        assertGt(OUTbBalanceAfter, OUTbBalanceBefore);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            TEST SWAP OUTb FOR ETH
+                            WITHOUT BRIDGING
+    //////////////////////////////////////////////////////////////*/
+
+    function test_swapOUTbForETH_dontBridgeTokens() public {
+        uint256 ethBalanceBefore = address(this).balance;
+        uint256 OUTbBalanceBefore = OUTbL1Token.balanceOf(address(this));
+
+        poolSwapAndBridgeOptimism.swap(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: false,
+                amountSpecified: -0.001 ether,
+                sqrtPriceLimitX96: TickMath.MAX_SQRT_PRICE - 1
+            }),
+            SwapAndBridgeOptimismRouter.SwapSettings({
+                bridgeTokens: false,
+                recipientAddress: address(this)
+            }),
+            ZERO_BYTES
+        );
+
+        uint256 ethBalanceAfter = address(this).balance;
+        uint256 OUTbBalanceAfter = OUTbL1Token.balanceOf(address(this));
+
+        assertGt(ethBalanceAfter, ethBalanceBefore);
+        assertEq(OUTbBalanceBefore - OUTbBalanceAfter, 0.001 ether);
+    }
+}
